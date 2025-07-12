a0172efa2d3897fe91406133ae571d6c9b8883e2
Amit Aharoni <ChefAharoni@proton.me>
2025-07-12 11:35:04 -0400

    Initial Commit

---
+++ ABCEatsTests/ABCEatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABCEatsTests.swift
+//  ABCEatsTests
+//
+//  Created by Amit Aharoni on 7/12/25.
+//
+
+import Testing
+@testable import ABCEats
+
+struct ABCEatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
